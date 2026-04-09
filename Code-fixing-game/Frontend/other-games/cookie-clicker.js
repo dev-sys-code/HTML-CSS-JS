db5f375fcf4612fc1ebb8f02489e61e94addff8e
dev-sys <Kaydenbuster@outlook.com>
2026-04-09 16:41:10 +0100

    Update cookie-clicker.js

--- Code-fixing-game/Frontend/other-games/cookie-clicker.js
+++ Code-fixing-game/Frontend/other-games/cookie-clicker.js
@@ -1,6 +1,14 @@
-count = 0;
+let count = 0;
+let saveCount = 0;
 
 function up() {
     count ++;
     document.getElementById("score").innerText = count;
+}
+
+function save() {
+    saveCount = count;
+}
+
+function selSave() {
 }
