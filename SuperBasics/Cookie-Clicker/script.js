97e585561f3c55e42076657577eba08522ea6f60
dev-sys <Kaydenbuster@outlook.com>
2026-04-14 20:50:20 +0100

    Create script.js

---
+++ SuperBasics/Cookie-Clicker/script.js
@@ -0,0 +1,11 @@
+let count = 0;
+
+function increment() {
+    count ++;
+    document.getElementById("score").innerHTML = count;
+}
+
+function reset() {
+    count = 0
+    document.getElementById("score").innerHTML = count;
+}
