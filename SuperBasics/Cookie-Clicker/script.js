f85890f51964370c2d65b48c9d91e628704a1a3a
dev-sys <Kaydenbuster@outlook.com>
2026-05-02 09:06:19 +0100

    Update script.js

--- SuperBasics/Cookie-Clicker/script.js
+++ SuperBasics/Cookie-Clicker/script.js
@@ -1,10 +1,9 @@
-let count = 0;
-
 function increment() {
     count ++;
     document.getElementById("score").innerHTML = count;
 }
 
+let count = 0;
 function reset() {
     count = 0;
     document.getElementById("score").innerHTML = count;
