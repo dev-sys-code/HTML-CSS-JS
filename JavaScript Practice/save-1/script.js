555a6612893cf648f1c7e2c158ffe4b0f85a9767
dev-sys <Kaydenbuster@outlook.com>
2026-05-04 13:53:44 +0100

    Update script.js

--- JavaScript Practice/save-1/script.js
+++ JavaScript Practice/save-1/script.js
@@ -1,7 +1,3 @@
-colours = ["#ffffff", "#000000", "#727272"]
-let colourIndex = 0;
-
-function background() {
-    let btn = document.body.style.background = colours[colourIndex]
-    colourIndex = (colourIndex + 1) % colours.length;
-}
+function black() { document.body.style.background = "linear-gradient(135deg, #403F44, #1E1B32)"; }
+function light() { document.body.style.background = "linear-gradient(135deg, #ffff, #f3f3f3)"; }
+function blue() { document.body.style.background = "linear-gradient(135deg, #5B7FFF, #33CCFC)"; }
